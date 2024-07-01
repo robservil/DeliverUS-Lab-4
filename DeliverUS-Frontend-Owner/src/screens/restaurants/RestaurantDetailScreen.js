8f0eabdde6d9e918916f385ce2dcd6351411a26e
Roberto <robservil@alumn.us.es>
2024-07-02 00:53:03 +0200

    prueba

--- DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/RestaurantDetailScreen.js
@@ -1,17 +1,58 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, View, FlatList, Pressable } from 'react-native'
+import { StyleSheet, View, FlatList, Pressable, ImageBackground, Image } from 'react-native'
 import { getDetail } from '../../api/RestaurantEndpoints'
 import TextRegular from '../../components/TextRegular'
+import TextSemiBold from '../../components/TextSemiBold'
 
 import * as GlobalStyles from '../../styles/GlobalStyles'
 
 export default function RestaurantDetailScreen ({ route }) {
-  const { id } = route.params
-  return (
-        <View style={styles.container}>
-            <TextRegular style={{ fontSize: 16, alignSelf: 'center', margin: 20 }}>Restaurant details. Id: {id}</TextRegular>
+  // const { id } = route.params
+  const [restaurant, setRestaurant] = useState({})
+
+  useEffect(() => {
+    console.log('Loading restaurant details, please wait 1 second')
+    setRestaurant(getDetail(route.params.id))
+  }, [])
+
+  const renderProduct = ({ item }) => {
+    return (
+      <Pressable
+        style={styles.row}
+        onPress={() => { }}>
+          <TextRegular>
+              {item.name}
+          </TextRegular>
+      </Pressable>
+    )
+  }
+
+  const renderHeader = () => {
+    return (
+      <ImageBackground source={(restaurant?.heroImage) ? { uri: process.env.API_BASE_URL + '/' + restaurant.heroImage, cache: 'force-cache' } : undefined } style={styles.imageBackground}>
+        <View style={styles.restaurantHeaderContainer}>
+            <TextSemiBold textStyle={styles.textTitle}>{restaurant.name}</TextSemiBold>
+            <Image style={styles.image} source={restaurant.logo ? { uri: process.env.API_BASE_URL + '/' + restaurant.logo, cache: 'force-cache' } : undefined} />
+            <TextRegular textStyle={styles.text}>{restaurant.description}</TextRegular>
         </View>
+      </ImageBackground>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+        <TextRegular style={styles.textTitle}>{restaurant.name}</TextRegular>
+        <TextRegular style={styles.text}>{restaurant.description}</TextRegular>
+        <TextRegular style={styles.text}>shippingCosts: {restaurant.shippingCosts}</TextRegular>
+        <FlatList
+          ListHeaderComponent={renderHeader}
+          style={styles.container}
+          data={restaurant.products}
+          renderItem={renderProduct}
+          keyExtractor={item => item.id.toString()}
+        />
+    </View>
   )
 }
 
@@ -24,13 +65,28 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     backgroundColor: GlobalStyles.brandSecondary
   },
-  textTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center'
+  restaurantHeaderContainer: {
+    height: 250,
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 100,
+    width: 100,
+    margin: 10
   },
   text: {
-    fontSize: 16,
-    textAlign: 'center'
+    color: 'white'
+  },
+  textTitle: {
+    fontSize: 20,
+    color: 'white'
   }
 })
